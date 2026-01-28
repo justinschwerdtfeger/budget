138dd86e52f52d0e84944322824c4d78c6c3e4cf
Justin <justinschwerdtfeger245@gmail.com>
2026-01-27 19:56:35 -0600

    Disable serwist on dev

--- next.config.js
+++ next.config.js
@@ -18,6 +18,7 @@ module.exports = async (phase) => {
       swSrc: "src/service-worker/app-worker.ts",
       swDest: "public/sw.js",
       reloadOnOnline: true,
+      disable: process.env.NODE_ENV === "development",
     });
     return withSerwist(nextConfig);
   }
